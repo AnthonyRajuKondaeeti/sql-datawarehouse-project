52589fab5c7a4fcf0e0d4212c028ce0819adafc2
AnthonyRajuKondaeeti <tonykondaveetijmj98@gmail.com>
2025-05-20 05:43:06 +0530

    Update insert.sql

--- scripts/insert.sql
+++ scripts/insert.sql
@@ -1,69 +1,56 @@
-TRUNCATE TABLE bronze.crm_cust_info;
-GO
-BULK INSERT bronze.crm_cust_info
-FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	TRUNCATE TABLE bronze.crm_cust_info;
+	BULK INSERT bronze.crm_cust_info
+	FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-GO
+	TRUNCATE TABLE bronze.crm_prd_info ;
+	BULK INSERT bronze.crm_prd_info 
+	FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.crm_prd_info ;
-GO
-BULK INSERT bronze.crm_prd_info 
-FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	TRUNCATE TABLE bronze.crm_sales_details;
+	BULK INSERT bronze.crm_sales_details
+	FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-GO
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.crm_sales_details;
-GO
-BULK INSERT bronze.crm_sales_details
-FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-GO
-
-TRUNCATE TABLE bronze.erp_cust_az12;
-GO
-BULK INSERT bronze.erp_cust_az12
-FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-GO
-
-TRUNCATE TABLE bronze.erp_loc_a101;
-GO
-BULK INSERT bronze.erp_loc_a101
-FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-GO 
-
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-GO
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR =',',
-	TABLOCK
-);
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'D:\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR =',',
+		TABLOCK
+	);
+END;
