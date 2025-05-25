f2e4195ce9c7fa7baa263ac08dcc35533d2a1060
AnthonyRajuKondaeeti <tonykondaveetijmj98@gmail.com>
2025-05-25 12:26:15 +0530

    Update EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -32,21 +32,27 @@ FROM gold.dim_customers
 --Find the Total Sales
 SELECT SUM(sales_amount) AS total_sales 
 FROM gold.fact_sales
+	
 --Find how many items are sold
 SELECT SUM(quantity) AS total_items 
 FROM gold.fact_sales
+	
 --Find the average selling price
 SELECT AVG(price) AS avg_selling_price 
 FROM gold.fact_sales
+	
 --Find the Total number of Orders
 SELECT COUNT(DISTINCT order_number) AS total_orders
 FROM gold.fact_sales
+	
 --Find the total no. of products
 SELECT COUNT(DISTINCT product_name) AS total_products
 FROM gold.dim_products
+	
 --Find the total no. of customers
 SELECT COUNT(DISTINCT customer_key) AS total_customers
 FROM gold.dim_customers
+	
 --Find the total no. of customers that has placed an order
 SELECT COUNT(DISTINCT customer_key)
 FROM gold.fact_sales
